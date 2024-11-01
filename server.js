e230cc57cbc4b55a21971d79ca33242431e05e8a
espinhara <gabrielespinhara13@gmail.com>
2024-11-01 00:40:30 -0300

    feat: ✨ import sale routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const productRoutes = require('./routes/productRoutes');
+const saleRoutes = require('./routes/saleRoutes');
 
 // Inicialização do app
 dotenv.config();
@@ -15,12 +16,13 @@ app.use(express.json());
 app.use('/uploads', express.static('uploads')); // Pasta para servir imagens
 
 // Conexão com MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI,)
   .then(() => console.log('Conectado ao MongoDB'))
   .catch(err => console.error('Erro ao conectar ao MongoDB:', err));
 
 // Rotas
 app.use('/api/products', productRoutes);
+app.use('/api/sales', saleRoutes);
 
 // Iniciar servidor
 app.listen(PORT, () => {
