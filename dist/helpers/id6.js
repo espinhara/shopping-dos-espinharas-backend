f56943bdc218b04a55417809b09d181a41c78a7f
espinhara <gabrielespinhara13@gmail.com>
2024-11-03 17:19:49 -0300

    refactor: 📦 removing dist

--- dist/helpers/id6.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = default_1;
-function default_1() {
-    return Math.floor(Math.random() * 2176782335).toString(36).toUpperCase().padStart(6, '0');
-}
