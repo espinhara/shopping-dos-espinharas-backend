8d22732800bf7ca054d07d5d1bc24c87abfbfc78
espinhara <gabrielespinhara13@gmail.com>
2024-11-10 01:45:42 -0300

    refactor: 📦 adding new possible type user on Request

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -3,7 +3,12 @@ import jwt from 'jsonwebtoken';
 declare global {
   namespace Express {
     interface Request {
-      user?: string | jwt.JwtPayload; // Dependendo do que você armazena no `user`
+      user?: string | {
+        id: string;
+        name: string;
+        email: string;
+        type: string;
+      } | jwt.JwtPayload; // Dependendo do que você armazena no `user`
     }
   }
 }
