23af66916be8e562cb99e87a9a55586956849787
espinhara <gabrielespinhara13@gmail.com>
2024-11-10 01:45:00 -0300

    feat: ✨ implements method authMiddleware to restrict access by type user

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -2,19 +2,29 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
-const authMiddleware = (req: Request, res: Response, next: NextFunction): void => {
-  const token = req.header('Authorization')?.replace('Bearer ', '');
-  if (!token) {
-    res.status(401).json({ message: 'Autorização negada' });
-    return;
-  }
+const authMiddleware = (requiredUserType: string) => {
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET ?? '');
-    req.user = decoded; // Você pode adicionar um tipo para req.user se desejar
-    next();
-  } catch (error) {
-    res.status(401).json({ message: 'Token inválido' });
+  return (req: Request, res: Response, next: NextFunction): void => {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+    if (!token) {
+      res.status(401).json({ message: 'Acesso negado. Token não fornecido.' });
+      return;
+    }
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET ?? '') as {
+        id: string;
+        name: string;
+        email: string;
+        type: string;
+      };
+      req.user = decoded; // Você pode adicionar um tipo para req.user se desejar
+      if (requiredUserType && req.user?.type !== requiredUserType)
+        res.status(401).json({ message: "Acesso negado. Tente mais tarde." })
+      next();
+    } catch (error) {
+      res.status(401).json({ message: 'Token inválido' });
+    }
   }
 };
 
