e60780d24fb0903e640d637b11aca0910c2b2d6c
espinhara <gabrielespinhara13@gmail.com>
2024-11-10 12:36:14 -0300

    refactor: 📦only client can to buy

--- src/routes/saleRoutes.ts
+++ src/routes/saleRoutes.ts
@@ -4,7 +4,7 @@ import { create, approve, list } from '../controllers/sale.controller';
 import authMiddleware from '../middleware/authMiddleware';
 const router = express.Router();
 
-router.post('/', create);  // Criando a compra
+router.post('/', authMiddleware('client'), create);  // Criando a compra
 router.patch('/:id/approve', authMiddleware('admin'), approve)
 router.get('/list', authMiddleware('admin'), list)
 
