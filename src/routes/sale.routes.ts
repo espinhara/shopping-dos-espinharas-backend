b810113dc403b5e61e82bd3ef8d04d98288d1c82
espinhara <gabrielespinhara13@gmail.com>
2024-11-20 15:18:04 -0300

    refactor: 📦 rename file name routes

--- src/routes/sale.routes.ts
+++ src/routes/sale.routes.ts
@@ -1,11 +1,12 @@
 // routes/sales.js
 import express from 'express';
-import { create, approve, list } from '../controllers/sale.controller';
+import { create, approve, list, total } from '../controllers/sale.controller';
 import authMiddleware from '../middleware/authMiddleware';
 const router = express.Router();
 
 router.post('/', authMiddleware('client'), create);  // Criando a compra
 router.patch('/:id/approve', authMiddleware('admin'), approve)
 router.get('/list', authMiddleware('admin'), list)
+router.get('/total', authMiddleware('admin'), total)
 
 export default router
