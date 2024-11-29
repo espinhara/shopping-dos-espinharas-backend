6b3f6cd87613d78e2f3ba62c5e8daf9695e29f6f
espinhara <gabrielespinhara13@gmail.com>
2024-11-28 23:47:41 -0300

    feat: ✨ adding new routes on users

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,6 +1,6 @@
 // routes/sales.js
 import express from 'express';
-import { list, update } from '../controllers/user.controller';
+import { countUsers, list, update } from '../controllers/user.controller';
 import authMiddleware from '../middleware/authMiddleware';
 const router = express.Router();
 
@@ -8,5 +8,6 @@ const router = express.Router();
 // router.patch('/:id/approve', authMiddleware, approve)
 router.get('/list', authMiddleware('admin'), list)
 router.put('/:id', update)
+router.get('/count', countUsers)
 
 export default router
