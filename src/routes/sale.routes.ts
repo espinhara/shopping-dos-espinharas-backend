1467bce8b7eb90f4c2485e8be79ef9bf9d8f831f
espinhara <gabrielespinhara13@gmail.com>
2024-12-02 00:06:29 -0300

    feat: ✨ adding route to access createFromCart

--- src/routes/sale.routes.ts
+++ src/routes/sale.routes.ts
@@ -1,6 +1,6 @@
 // routes/sales.js
 import express from 'express';
-import { create, approve, list, total, getSalesLastSixMonths, getProductsSalesData, getTopAndLeastSoldProducts } from '../controllers/sale.controller';
+import { create, approve, list, total, getSalesLastSixMonths, getProductsSalesData, getTopAndLeastSoldProducts, createFromCart } from '../controllers/sale.controller';
 import authMiddleware from '../middleware/authMiddleware';
 const router = express.Router();
 
@@ -11,5 +11,6 @@ router.get('/total', authMiddleware('admin'), total)
 router.get('/last-six-months', authMiddleware('admin'), getSalesLastSixMonths)
 router.get('/products-per-sales', authMiddleware('admin'), getProductsSalesData)
 router.get('/top-least-products', authMiddleware('admin'), getTopAndLeastSoldProducts)
+router.post('/cart', authMiddleware('client'), createFromCart)
 
 export default router
