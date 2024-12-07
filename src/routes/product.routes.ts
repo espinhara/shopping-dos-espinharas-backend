c0a9d86a1d513755592ef9143e1ee0e34fa9e0d9
espinhara <gabrielespinhara13@gmail.com>
2024-12-07 18:47:01 -0300

    feat: ✨ adding route to access search method

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { createProduct, getProducts, getProductById, updateProduct, deleteProduct, list } from '../controllers/product.controller';
+import { createProduct, getProducts, getProductById, updateProduct, deleteProduct, list, getPaginatedProducts } from '../controllers/product.controller';
 import upload from '../middleware/multer';
 import authMiddleware from '../middleware/authMiddleware';
 
@@ -9,7 +9,8 @@ const router = express.Router();
 // Rotas de produtos
 router.post('/', authMiddleware('admin'), upload.array('images', 4), createProduct);  // Aceitar até 4 imagens
 router.get('/', getProducts);
-router.get('/list', authMiddleware('admin'), (req: Request, res: Response) => list(req, res));
+router.get('/list', authMiddleware('admin'), list);
+router.get('/search', getPaginatedProducts)
 router.get('/:id', getProductById);
 router.put('/:id', authMiddleware('admin'), upload.array('images', 4), updateProduct);  // Aceitar até 4 imagens
 router.delete('/:id', authMiddleware('admin'), deleteProduct);
